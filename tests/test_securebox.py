6d3c7c18fff9c4b8ecb72795386232cec294da1a
Simone Perini <39993316+CoffeePerry@users.noreply.github.com>
2020-11-15 17:46:08 +0100

    More automated tests

--- tests/test_securebox.py
+++ tests/test_securebox.py
@@ -2,7 +2,7 @@
 
 from pysecurebox.securebox import SecureBox
 
-from os import path, makedirs
+from os import path, makedirs, remove
 
 DIR_INSTANCE = 'instance'
 FILE_TEST = path.join(DIR_INSTANCE, 'test.box')
@@ -12,6 +12,8 @@ PASSWORD = 'secret'
 def test_secure_box_write_read():
     if not path.isdir(DIR_INSTANCE):
         makedirs(DIR_INSTANCE)
+    if path.exists(FILE_TEST):
+        remove(FILE_TEST)
     secure_box = SecureBox(FILE_TEST, PASSWORD)
     secure_box.append(b'test_1')
     assert (path.exists(FILE_TEST))
