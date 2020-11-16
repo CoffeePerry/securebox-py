ddf892a5bfe54bd9de0fecd0101f66f33412ad3c
Simone Perini <39993316+CoffeePerry@users.noreply.github.com>
2020-11-16 20:00:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,21 @@ setup(
     name='pysecurebox',
     packages=find_packages(include=['pysecurebox']),
     version='1.0.0',
-    description='',
+    description='securebox-py (SecureBox for Python) is a lightweight package to create and manage consistent, '
+                'encrypted and authenticated files.',
+    keywords=['security', 'files', 'encryption', 'authentication'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Security :: Cryptography',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],  # https://pypi.org/classifiers/
     author='Simone Perini',
     author_email='perinisimone98@gmail.com',
-    url='https://github.com/CoffeePerry/securebox-py',
     license='MIT',
+    url='https://github.com/CoffeePerry/securebox-py',
+    download_url='https://github.com/CoffeePerry/securebox-py',
     install_requires=['pycryptodomex==3.9.9'],
     setup_requires=['pytest-runner==5.2'],
     tests_require=['pytest==4.4.1'],
